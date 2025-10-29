d95d3a29ccfd1636bd34b6f3fc04769bb6b40293
PaRaShi12 <82266062+PaRaShi12@users.noreply.github.com>
2025-10-29 18:32:37 +0000

    Deploying to gh-pages from @ PaRaShi12/PaRaShi12.github.io@94b39f74d6bc20f9ae4a59224cc6a70f7a6521ac 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "PaRaShi12/Kafashi",
+    "data-repo": "PaRaShi12/PaRaShi12.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
